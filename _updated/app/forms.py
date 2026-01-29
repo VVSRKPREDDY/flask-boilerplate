9268452aa34546efa86c0a8e81babb3f98a313c5
VVSRKPREDDY <134038938+VVSRKPREDDY@users.noreply.github.com>
2026-01-29 12:08:36 +0530

    Update forms.py

--- _updated/app/forms.py
+++ _updated/app/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import Form
-from wtforms import TextField, PasswordField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, EqualTo, Length
 
 
 class RegisterForm(Form):
-    name = TextField(
+    name = StringField(
         'Username', validators=[DataRequired(), Length(min=6, max=25)]
     )
     email = TextField(
